7bab6639d20d83c840a8c3252a30794b54462f67
ThaniswarN <thaniswarn@gmail.com>
2026-06-01 21:44:00 +0530

    database.sql

--- database.sql
+++ database.sql
@@ -23,3 +23,25 @@ CREATE TABLE transactions (
     return_date DATE,
     status VARCHAR(20)
 );
+
+CREATE TABLE Books (
+    book_id INT PRIMARY KEY,
+    title VARCHAR(100),
+    author VARCHAR(100),
+    status VARCHAR(20)
+);
+
+CREATE TABLE Students (
+    student_id INT PRIMARY KEY,
+    student_name VARCHAR(100),
+    department VARCHAR(100)
+);
+
+CREATE TABLE Transactions (
+    transaction_id INT PRIMARY KEY,
+    student_id INT,
+    book_id INT,
+    issue_date DATE,
+    return_date DATE
+);
+
